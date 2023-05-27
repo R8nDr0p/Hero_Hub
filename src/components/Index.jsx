b52fd5ee7ef9ffa4026bc4fa8aea58b5f0fac9b8
R8nDr0p <rainchrstn@gmail.com>
2023-05-27 21:35:37 +0800

    added components index and navbar

---
+++ src/components/Index.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Index() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default Index;
